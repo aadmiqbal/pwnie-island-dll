6443ab2dfedd92902266370d925cd32cbc0134a2
Efe Suner <ses907@student.bham.ac.uk>
2023-11-18 22:57:05 +0000

    Implemented various hacks through chat functionality

--- dll/dllmain.cpp
+++ dll/dllmain.cpp
@@ -4,9 +4,23 @@
 #include <iostream> 
 #include <tlhelp32.h>
 #include <vector>
-
+#include <string>
 
 DWORD procId = GetCurrentProcessId();
+
+std::string getLastChar(std::string s, std::string delimiter)
+{
+	size_t pos = 0;
+	std::string token;
+
+	while ((pos = s.find(delimiter)) != (std::string::npos))
+	{
+		token = s.substr(0, pos);
+		s.erase(0, pos + delimiter.length());
+	}
+
+	return s;
+}
 
 // PatchByte and LocateDirectMemoryAddress Components modified from https://guidedhacking.com/threads/how-to-hack-any-game-tutorial-c-trainer-1-external.10897/
 
@@ -17,6 +31,7 @@ void PatchByte(BYTE* dst, BYTE* src, unsigned int size) {
 	memcpy(dst, src, size);
 	VirtualProtect(dst, size, oldProtect, &oldProtect);
 }
+
 uintptr_t LocateDirectMemoryAddress(uintptr_t ptr, std::vector<unsigned int> offsets) {
 	uintptr_t address = ptr;
 
@@ -52,14 +67,15 @@ uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
 	return modBaseAddr;
 }
 
-uintptr_t moduleBase = GetModuleBaseAddress(procId, L"GameLogic.dll");
+// Placeholder for the runtime base address of "GameLogic.dll" and "PwnAdventure3.exe"
+uintptr_t runtimeBaseAddress = GetModuleBaseAddress(procId, L"GameLogic.dll");
+uintptr_t PwnAdventAddr = (uintptr_t)GetModuleHandle(L"PwnAdventure3-Win32-Shipping.exe");
 
 typedef bool(__thiscall* OriginalCanJump)(void* thisPtr);
 OriginalCanJump trampolineCanJump = nullptr;
 
 //custom CanJump function
 bool __fastcall MyCanJump(void* thisPtr) {
-	printf("working");
 	return 1;
 }
 
@@ -81,10 +97,85 @@ void HookCanJump() {
 	// Restore original memory protection
 	VirtualProtect((LPVOID)canJumpAddr, 5, oldProtect, &oldProtect);
 }
-// Placeholder for the runtime base address of "GameLogic.dll"
-uintptr_t runtimeBaseAddress = GetModuleBaseAddress(procId, L"GameLogic.dll");
 
-uintptr_t offset = 0x551a0; // Correct offset for Player::Chat
+void setMana(int newManaValue) {
+	printf("PwnAventAddr: %p\n", PwnAdventAddr);
+
+	// Offset for the mana pointer
+	uintptr_t manaPointerOffset = 0x18FCD60;
+
+	// Adding offsets one by one
+	uintptr_t firstPointer = *(uintptr_t*)(PwnAdventAddr + manaPointerOffset);
+	printf("PwnAventAddr + 0x18FCD60 = %p has value %p\n", PwnAdventAddr + manaPointerOffset, firstPointer);
+
+	uintptr_t secondPointer = *(uintptr_t*)(firstPointer + 0x20);
+	printf("firstPointer + 0x20 = %p has value %p\n", firstPointer + 0x20, secondPointer);
+
+	uintptr_t thirdPointer = *(uintptr_t*)(secondPointer + 0x284);
+	printf("secondPointer + 0x284 = %p has value %p\n", secondPointer + 0x284, thirdPointer);
+
+	uintptr_t fourthPointer = *(uintptr_t*)(thirdPointer + 0x3E0);
+	printf("thirdPointer + 0x3E0 = %p has value %p\n", thirdPointer + 0x3E0, fourthPointer);
+
+	int *manaValue = (int*)(fourthPointer + 0xBC);
+	printf("fourthPointer + 0xBC = %p has value %d\n", fourthPointer + 0xBC, *manaValue);
+
+	int mana = *manaValue;
+	std::cout << "\nCurrent mana value: " << mana;
+
+	*manaValue = newManaValue;
+	std::cout << "Mana value set to: " << newManaValue << "\n\n";
+}
+
+void setWalkSpeed(float newWalkSpeed)
+{
+	float *walkSpeed = (float*)LocateDirectMemoryAddress(runtimeBaseAddress + 0x00097D7C, { 0x1C, 0x6c, (0xbc + 0x64) });
+	float walkSpeed = *walkSpeed;
+	std::cout << "\nCurrent walk speed value: " << walkSpeed;
+
+	*walkSpeed = newWalkSpeed;
+	std::cout << "Walk speed value set to: " << newWalkSpeed << "\n\n";
+}
+
+void setJumpSpeed(float newJumpSpeed)
+{
+	float *jumpSpeed = (float*)LocateDirectMemoryAddress(runtimeBaseAddress + 0x00097D7C, { 0x1C, 0x6c, (0xbc + 0x68) });
+	float jumpSpeed = *jumpSpeed;
+	std::cout << "\nCurrent jump speed value: " << jumpSpeed;
+
+	*jumpSpeed = newJumpSpeed;
+	std::cout << "Jump speed value set to: " << newJumpSpeed << "\n\n";
+}
+
+void setXCoord(float newXCoord) {
+	float *xCoord = (float*)LocateDirectMemoryAddress(runtimeBaseAddress + 0x00097E1C, { 0x24, 0xc, 0xf8, 0x18, 0x2fc, 0x280, 0x90 });
+	float xCoord = *xCoord;
+	std::cout << "\nCurrent x coord value: " << xCoord;
+
+	*xCoord = newXCoord;
+	std::cout << "X coord set to: " << newXCoord << "\n\n";
+}
+
+void setYCoord(float newYCoord) {
+	float *yCoord = (float*)LocateDirectMemoryAddress(runtimeBaseAddress + 0x00097E1C, { 0x24, 0xc, 0xf8, 0x18, 0x2fc, 0x280, 0x94 });
+	float yCoord = *yCoord;
+	std::cout << "\nCurrent y coord value: " << yCoord;
+
+	*yCoord = newYCoord;
+	std::cout << "Y coord set to: " << newYCoord << "\n\n";
+}
+
+void setZCoord(float newZCoord) {
+	float *zCoord = (float*)LocateDirectMemoryAddress(runtimeBaseAddress + 0x00097E1C, { 0x24, 0xc, 0xf8, 0x18, 0x2fc, 0x280, 0x98 });
+	float zCoord = *zCoord;
+	std::cout << "\nCurrent z coord value: " << zCoord;
+
+	*zCoord = newZCoord;
+	std::cout << "Y coord set to: " << zCoord << "\n\n";
+}
+
+// Correct offset for Player::Chat
+uintptr_t offset = 0x551a0;
 
 // Calculate the actual runtime address of the Chat function
 uintptr_t chatFuncAddr = runtimeBaseAddress + offset;
@@ -97,15 +188,50 @@ typedef void(__thiscall* ChatFuncType)(const char* text);
 
 // Custom function to intercept and modify the Chat function behavior
 void __fastcall MyCustomChat(void* thisPlayer, ChatFuncType func, const char* originalText) {
-	printf(originalText);
+	std::cout << "Chat input string: " << originalText;
 
-	// Compare originalText to the string "adam" using strcmp
-	if (strcmp(originalText, "spaceinvaders") == 0) {
-		printf("Space Invaders started");
+	std::string originalTextStr = originalText;
+
+	// Commands to start a hack
+	if (strcmp(originalText, "init spaceInvaders") == 0) {
+		std::cout << "Space Invaders started";
+		
+	}
+	else if (strcmp(originalText, "init trampoline") == 0) {
+		std::cout << "Trampoline hack started";
 		HookCanJump();
 	}
+	else if (originalTextStr.rfind("set mana", 0) == 0) {
+		std::cout << "Mana hack started";
+		int newManaValue = stoi(getLastChar(originalTextStr, " "));
+		setMana(newManaValue);
+	}
+	else if (originalTextStr.rfind("set walkSpeed", 0) == 0) {
+		std::cout << "Walk speed hack started";
+		float newWalkSpeed = stof(getLastChar(originalTextStr, " "));
+		setWalkSpeed(newWalkSpeed);
+	}
+	else if (originalTextStr.rfind("set jumpSpeed", 0) == 0) {
+		std::cout << "Jump speed hack started";
+		float newJumpSpeed = stof(getLastChar(originalTextStr, " "));
+		setJumpSpeed(newJumpSpeed);
+	}
+	else if (originalTextStr.rfind("set x", 0) == 0) {
+		std::cout << "X coordinate hack started";
+		float newXCoord = stof(getLastChar(originalTextStr, " "));
+		setXCoord(newXCoord);
+	}
+	else if (originalTextStr.rfind("set y", 0) == 0) {
+		std::cout << "Y coordinate hack started";
+		float newYCoord = stof(getLastChar(originalTextStr, " "));
+		setYCoord(newYCoord);
+	}
+	else if (originalTextStr.rfind("set z", 0) == 0) {
+		std::cout << "Z coordinate hack started";
+		float newZCoord = stof(getLastChar(originalTextStr, " "));
+		setZCoord(newZCoord);
+	}
 }
-
 
 void HookChatFunction() {
 	std::cout << "HookChatFunction: Starting." << std::endl;
@@ -136,15 +262,6 @@ void HookChatFunction() {
 	std::cout << "HookChatFunction: Original Chat function address saved." << std::endl;
 }
 
-
-void InitializeHooks() {
-	HookChatFunction();
-}
-
-
-
-
-
 // This method defines a threat that will run concurrently with the game
 DWORD WINAPI MyThread(HMODULE hModule)
 {
@@ -155,87 +272,8 @@ DWORD WINAPI MyThread(HMODULE hModule)
 
 	std::cout << "Injection worked\n";
 	std::cout << "Process ID is: " << GetCurrentProcessId() << std::endl;
-
-	//Mana hack start
-	uintptr_t PwnAventAddr = (uintptr_t)GetModuleHandle(L"PwnAdventure3-Win32-Shipping.exe");
-	printf("PwnAventAddr: %p\n", PwnAventAddr);
-
-	// Offset for the mana pointer
-	uintptr_t manaPointerOffset = 0x18FCD60;
-
-	// Adding offsets one by one
-	uintptr_t firstPointer = *(uintptr_t*)(PwnAventAddr + manaPointerOffset);
-	printf("PwnAventAddr + 0x18FCD60 = %p has value %p\n", PwnAventAddr + manaPointerOffset, firstPointer);
-
-	uintptr_t secondPointer = *(uintptr_t*)(firstPointer + 0x20);
-	printf("firstPointer + 0x20 = %p has value %p\n", firstPointer + 0x20, secondPointer);
-
-	uintptr_t thirdPointer = *(uintptr_t*)(secondPointer + 0x284);
-	printf("secondPointer + 0x284 = %p has value %p\n", secondPointer + 0x284, thirdPointer);
-
-	uintptr_t fourthPointer = *(uintptr_t*)(thirdPointer + 0x3E0);
-	printf("thirdPointer + 0x3E0 = %p has value %p\n", thirdPointer + 0x3E0, fourthPointer);
-
-	int* manaValue = (int*)(fourthPointer + 0xBC);
-	printf("fourthPointer + 0xBC = %p has value %d\n", fourthPointer + 0xBC, *manaValue);
-
-	std::cout << "\nMana: " << *manaValue << "\n\n";
-
-	//Z coordinate hack - Tom
-	printf("PwnAventAddr: %p\n", PwnAventAddr);
-	uintptr_t firstStep = *(uintptr_t*)(PwnAventAddr + 0x18FCD60);
-	printf("PwnAventAddr + 0x18FCD60 = %p has value %p\n", PwnAventAddr + 0x18FCD60, firstStep);
-	uintptr_t secondStep = *(uintptr_t*)(firstStep + 0x20);
-	printf("firstStep + 0x20 = %p has value %p\n", firstStep + 0x20, secondStep);
-	uintptr_t thirdStep = *(uintptr_t*)(secondStep + 0x238);
-	printf("secondStep + 0x238 = %p has value %p\n", secondStep + 0x238, thirdStep);
-	uintptr_t forthStep = *(uintptr_t*)(thirdStep + 0x280);
-	printf("thirdStep + 0x280 = %p has value %p\n", thirdStep + 0x280, forthStep);
-
-	float* z_coord_Address = (float*)(forthStep + 0x98);
-	float z_coord = *z_coord_Address;
-
-	// This is the main loop that will run in the background while I play the game
-	while (true) {
-		// If the player z coordinate (height) changes then print it.
-		if (z_coord != *z_coord_Address) {
-			z_coord = *z_coord_Address;
-			//std::cout << " Z co-ord: " << z_coord << std::endl;
-		}
-
-		// If the player presses 'F' then add 10000 to the players heigth
-		// I.e. this makes 'F' a super jump key that will let me jump through solid objects 
-		if (GetAsyncKeyState('F') & 1) {
-			std::cout << "   F key pressed";
-
-			int mana = *(int*)LocateDirectMemoryAddress(moduleBase + 0x00097D7C, { 0x1C, 0x6c, 0xBC });
-			printf("\n mana value is %d \n", mana);
-
-			float walkspeed = *(float*)LocateDirectMemoryAddress(moduleBase + 0x00097D7C, { 0x1C, 0x6c, (0xbc + 0x64) });
-			printf("\n walkspeed value is %f \n", walkspeed);
-
-			float jumpspeed = *(float*)LocateDirectMemoryAddress(moduleBase + 0x00097D7C, { 0x1C, 0x6c, (0xbc + 0x68) });
-			printf("\n jumpspeed value is %f \n", jumpspeed);
-
-			float jumpholdtime = *(float*)LocateDirectMemoryAddress(moduleBase + 0x00097D7C, { 0x1C, 0x6c, (0xbc + 0x68 + 0x4) });
-			printf("\n jumpholdtime value is %f \n", jumpholdtime);
-
-			float xcoord = *(float*)LocateDirectMemoryAddress(moduleBase + 0x00097E1C, { 0x24, 0xc, 0xf8, 0x18, 0x2fc, 0x280, 0x90 });
-			printf("\n X-coord value is %f \n", xcoord);
-
-			float ycoord = *(float*)LocateDirectMemoryAddress(moduleBase + 0x00097E1C, { 0x24, 0xc, 0xf8, 0x18, 0x2fc, 0x280, 0x94 });
-			printf("\n Y-coord value is %f \n", ycoord);
-
-			float zcoord = *(float*)LocateDirectMemoryAddress(moduleBase + 0x00097E1C, { 0x24, 0xc, 0xf8, 0x18, 0x2fc, 0x280, 0x98 });
-			printf("\n Z-coord value is %f \n", zcoord);
-
-			*z_coord_Address = *z_coord_Address + 10000;
-		}
-
-
-	}
-
-	return 0;
+	
+	HookChatFunction();
 }
 
 // This is the main method that runs when the DLL is injected.
@@ -249,7 +287,6 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	case DLL_PROCESS_ATTACH:
 	{
 		DisableThreadLibraryCalls(hModule);
-		InitializeHooks();
 		// We run the cheat code in a seperate thread to stop it interupting the game execution. 
 		// Again we dont catch a possible NULL, if we are going down then we can go down in flames. 
 		CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MyThread, hModule, 0, nullptr));
